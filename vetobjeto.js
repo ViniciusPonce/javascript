e793d46e0a791823f993d06ed2af884be9a5974a
VINÍCIUS PONCE CINTRA <22016@unifacef.edu.br>
2019-08-21 21:46:17 -0300

    vetor de objetos v 0.2

--- vetobjeto.js
+++ vetobjeto.js
@@ -1,3 +1,13 @@
 function funcao(){
-    
+    var vet = []
+    for(var i=0; i<5; i++){
+    var objeto = {}
+    // coloca dados no objeto
+    objeto.codigo = Number(prompt("Informe o codigo: "))
+    objeto.nome = prompt("Informe o Nome: ")
+    objeto.quantidade = Number(prompt("Informe a quantidade: "))
+    objeto.preco = Number(prompt("Informe o preço: "))
+    //coloca objeto no vetor
+    vet.push(objeto)
+    }
 }
