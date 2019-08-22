2bd9f4b6f2d332354f1ab94786d60ff0c8decd7a
VINÍCIUS PONCE CINTRA <22016@unifacef.edu.br>
2019-08-21 22:08:39 -0300

    vetor de objetos v 1.0

--- vetobjeto.js
+++ vetobjeto.js
@@ -1,13 +1,20 @@
 function funcao(){
-    var vet = []
     for(var i=0; i<5; i++){
     var objeto = {}
     // coloca dados no objeto
-    objeto.codigo = Number(prompt("Informe o codigo: "))
-    objeto.nome = prompt("Informe o Nome: ")
-    objeto.quantidade = Number(prompt("Informe a quantidade: "))
-    objeto.preco = Number(prompt("Informe o preço: "))
-    //coloca objeto no vetor
-    vet.push(objeto)
+        objeto.codigo = Number(prompt("Informe o codigo: "))
+        objeto.nome = prompt("Informe o Nome: ")
+        objeto.quantidade = Number(prompt("Informe a quantidade: "))
+        objeto.preco = Number(prompt("Informe o preço: "))
+        //coloca objeto no vetor
+        vet.push(objeto)
     }
+    //calcula estoque
+    var estoque = 0
+    for (var i=0; i<5; i++){
+        estoque = estoque + vet[i].quantidade
+    }
+       
+
+    alert("O valor total em estoque é" + estoque)
 }
