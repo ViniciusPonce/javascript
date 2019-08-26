88bfcd374c65273e27a0cfdff2fa149be6b82fa7
VINÍCIUS PONCE CINTRA <22016@unifacef.edu.br>
2019-08-26 20:27:23 -0300

    Adicionando exercicio 2

--- vetobjeto.js
+++ vetobjeto.js
@@ -1,20 +1,33 @@
 function funcao(){
-    for(var i=0; i<5; i++){
+    var vet = []
+    for(var i=0; i<3; i++){
     var objeto = {}
     // coloca dados no objeto
-        objeto.codigo = Number(prompt("Informe o codigo: "))
-        objeto.nome = prompt("Informe o Nome: ")
-        objeto.quantidade = Number(prompt("Informe a quantidade: "))
-        objeto.preco = Number(prompt("Informe o preço: "))
+        objeto.salario = Number(prompt("Informe o salario do habitante: "))
+        objeto.idade = Number(prompt("Informe a idade do habitante: "))
+        objeto.nfilhos = Number(prompt("Informe o numero de filhos do habitante: "))
+        objeto.sexo = (prompt("Informe o sexo do habitante: "))
         //coloca objeto no vetor
         vet.push(objeto)
     }
     //calcula estoque
-    var estoque = 0
-    for (var i=0; i<5; i++){
-        estoque = estoque + vet[i].quantidade
+    var somaSalario = 0
+    var somaFilhos = 0
+    var maiorSalario = vet[0].salario
+    for (var i=0; i<vet.length; i++){
+        somaSalario = somaSalario + vet[i].salario;
+        somaFilhos = somaFilhos + vet[i].nfilhos
+        if (vet[i].salario > marioSalario){
+            maiorSalario = vet[i].salario
+        }
+        if (vet[i].sexo == 'F') && (vet[i].salario > 1000)
+            qtde++;
+        
     }
        
 
-    alert("O valor total em estoque é" + estoque)
+    alert("A media de salario da população é: " + somaSalario/vet.length)
+    alert("A media de numero de filhos é " + somaFilhos/vet.length)
+    alert("O maior salario é de: " + maiorSalario)
+    alert("Percentual de mulheres com salario maior de R$1000,00" + qtde/3*100);
 }
